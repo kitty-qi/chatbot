26307c41986aae104d02b596449edcba5543e44b
Hui Qi <102757997+kitty-qi@users.noreply.github.com>
2024-06-04 23:27:59 +0800

    Update Fitness_Nutrition_Chatbot.py

--- Fitness_Nutrition_Chatbot.py
+++ Fitness_Nutrition_Chatbot.py
@@ -12,6 +12,7 @@ from htmlTemplates import bot_template, user_template, css
 #from transformers import pipeline
 import nltk
 nltk.download('punkt')
+nltk.download('wordnet')
 from stop_words import get_stop_words
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
